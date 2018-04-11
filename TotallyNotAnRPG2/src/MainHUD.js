e692dda93c24d9bf36dcc852dccedae5fe209e34
TheLampost <34274706+TheLampost@users.noreply.github.com>
2018-04-11 17:48:00 -0400

    lil updates pt.2
    uh

--- TotallyNotAnRPG2/src/MainHUD.js
+++ TotallyNotAnRPG2/src/MainHUD.js
@@ -7,6 +7,9 @@ var HUD
 var mouseX;
 var mouseY;
 
+canvas.addEventListener("mousemove", checkPos);
+canvas.addEventListener("mouseup", checkClick);
+window.addEventListener("keydown", onKeyDown);
 
 HUDimage();
 
@@ -27,4 +30,35 @@ function update()
 	render();
 }
 uInt =setInterval(update,33.34);
+
+function checkPos (mouseEvent){
+	if(mouseEvent.pageX || mouseEvent.pageY == 0){
+		mouseX = mouseEvent.pageX - this.offsetLeft;
+		mouseY = mouseEvent.pageY - this.offsetTop;
+	}else if (mouseEvent.offsetX || mouseEvent.offsetY == 0){
+		mouseX = mouseEvent.offsetX;
+		mouseY = mouseEvent.offsetY;
+	}
+}
+
+function checkClick(mouseEvent){
+	if (mouseX > 750 && mouseX < 878 && mouseY > 600 && mouseY < 720){
+			LoadContinue();
+			canvas.removeEventListener("mousemove", checkPos);
+			canvas.removeEventListener("mouseup", checkClick);
+		}
+
+	
+	}	
+	
+	function onKeyDown(event)
+	{
+			console.log("hi");
+		switch(event.keyCode)
+		{
+			case 65://A
+			LoadGameOver();
+			break;
+		}
+	}
 
